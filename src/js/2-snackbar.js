473dfa4ce8d74a81696428c9fc69b9bfa7aafaa0
Andrii Storozhenko <andreystorozhenko123456@gmail.com>
2025-12-06 16:05:09 +0100

    add snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,33 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
 console.log("Snackbar");
+
+const form = document.querySelector("form");
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const delay =  form.elements.delay.value;
+    const state = form.elements.state.value;
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => { 
+            if (state === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+
+    promise
+        .then(delay => iziToast.success({ message: `Fulfilled promise in ${delay}ms`, position: "topRight", }))
+        .catch(error =>  iziToast.error({  message: `Rejected promise in ${delay}ms`, position: "topRight", }))
+
+});
+
+
